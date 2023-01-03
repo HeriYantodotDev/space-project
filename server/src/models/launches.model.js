78803b1bea5726700c276f42670a8dba6b7b3b37
HeriYantodotDev <heri@HYA>
2023-01-03 16:30:41 +0700

    the design of launches models

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+const launches = new Map();
+
+const launch = { 
+    flightNumber: 100,
+    mission: 'Yoda Exploration II',
+    rocket : 'Lightsaber Blue I',
+    launchDate : new Date('August 17, 2050') ,
+    destination : 'Kepler-442 b',
+    customer : ['Jedi', 'Sith', 'Galactic Empire'],
+    upcoming: true,
+    success: true
+}
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+    launches
+}
